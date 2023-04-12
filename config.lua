7f139acbcd80b5244d441077e3b9f4635c62fd2f
patriot720 <cerkin-3@yandex.ru>
2023-04-12 18:31:20 +0300

    Personal config

--- config.lua
+++ config.lua
@@ -1,11 +1,13 @@
 -- doom_config - Doom Nvim user configurations file
 --
+--
 -- This file contains the user-defined configurations for Doom nvim.
 -- Just override stuff in the `doom` global table (it's injected into scope
 -- automatically).
 
 -- ADDING A PACKAGE
 --
+doom.use_package("AndrewRadev/bufferize.vim");
 -- doom.use_package("EdenEast/nightfox.nvim", "sainnhe/sonokai")
 -- doom.use_package({
 --   "ur4ltz/surround.nvim",
@@ -16,17 +18,46 @@
 
 -- ADDING A KEYBIND
 --
--- doom.use_keybind({
---   -- The `name` field will add the keybind to whichkey
---   {"<leader>s", name = '+search', {
---     -- Bind to a vim command
---     {"g", "Telescope grep_string<CR>", name = "Grep project"},
---     -- Or to a lua function
---     {"p", function()
---       print("Not implemented yet")
---     end, name = ""}
---   }}
--- })
+doom.features.lsp.settings.completion.mapping = function(cmp)
+return {
+    ["<Down>"] = cmp.mapping.select_next_item(),
+    ["<Up>"] = cmp.mapping.select_prev_item()
+  }
+end
+table.insert(doom.features.telescope.binds, {
+  "<leader><space>","<cmd>Telescope find_files<CR>", name = "Find in project"
+})
+
+doom.features.explorer.binds = {
+  { "<F3>", ":NvimTreeToggle<CR>", name = "Toggle file explorer" },
+  {
+    "<leader>",
+    name = "+prefix",
+    {
+      {
+        "o",
+        name = "+open/close",
+        {
+          { "p", "<cmd>NvimTreeToggle<CR>", name = "Explorer" },
+        },
+      },
+    },
+  },
+}
+
+doom.use_keybind({
+  -- The `name` field will add the keybind to whichkey
+  {"<leader>s", name = '+search', {
+    -- Bind to a vim command
+    {"g", "Telescope grep_string<CR>", name = "Grep project"},
+    -- Or to a lua function
+    {"p", function()
+      print("Not implemented yet")
+    end, name = ""}
+  }}
+})
+vim.o.guifont = "JetBrains Mono:h10"
+doom.relative_num = false;
 
 -- ADDING A COMMAND
 --
--- modules.lua
+++ modules.lua
@@ -30,28 +30,28 @@ return {
 
     -- UI Components
     "lsp_progress", -- Check status of LSP loading
-    "tabline", -- Tab bar buffer switcher
+    -- "tabline", -- Tab bar buffer switcher
     "dashboard", -- A pretty dashboard upon opening
-    -- "trouble",         -- A pretty diagnostic viewer
+    "trouble",         -- A pretty diagnostic viewer
     "statusline", -- A pretty status line at the bottom of the buffer
     -- "minimap",         -- Shows current position in document
-    -- "terminal",        -- Integrated terminal in neovim
-    -- "symbols",         -- Navigate between code symbols using telescope
-    -- "ranger",          -- File explorer in neovim (TODO: Test)
+    "terminal",        -- Integrated terminal in neovim
+    "symbols",         -- Navigate between code symbols using telescope
+    "ranger",          -- File explorer in neovim (TODO: Test)
     -- "restclient",      -- Test HTTP requests from neovim (TODO: Test)
-    -- "show_registers",  -- Show and navigate between registers
+    "show_registers",  -- Show and navigate between registers
 
     -- Tools
-    -- "dap",             -- Debug code through neovim
+    "dap",             -- Debug code through neovim
     "repl", -- Interactive REPL in neovim
     "explorer", -- An enhanced filetree explorer
-    -- "firenvim",        -- Embed neovim in your browser
-    -- "lazygit",         -- Lazy git integration
-    -- "neogit",          -- A git client for neovim
+    "firenvim",        -- Embed neovim in your browser
+    "lazygit",         -- Lazy git integration
+    "neogit",          -- A git client for neovim
     "neorg", -- Organise your life
     "projects", -- Quickly switch between projects
-    -- "superman",        -- Read unix man pages in neovim
-    -- "suda",            -- Save using sudo when necessary
+    "superman",        -- Read unix man pages in neovim
+    "suda",            -- Save using sudo when necessary
     "telescope", -- Fuzzy searcher to find files, grep code and more
     "whichkey", -- An interactive sheet
   },
